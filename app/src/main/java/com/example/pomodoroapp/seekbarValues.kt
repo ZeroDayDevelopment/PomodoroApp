b1a61e8626f862d139b57553871e59e55338dae6
ZeroDayDevelopment <salihsafahome@gmail.com>
2023-04-26 13:47:48 +0300

    version 1.2

---
+++ app/src/main/java/com/example/pomodoroapp/seekbarValues.kt
@@ -0,0 +1,8 @@
+package com.example.pomodoroapp
+
+class seekbarValues{
+    var work_seekbar:Int = 25*60000
+    var short_break_seekbar:Int = 5*60000
+    var long_break_seekbar:Int = 15*60000
+    var session_seekbar:Int = 4
+}
